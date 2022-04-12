e086397a9d04fea13fefe48dc93ebb101b194a99
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2022-04-12 12:49:03 +0600

    fixed svg issue

--- src/components/Congestion.js
+++ src/components/Congestion.js
@@ -2,13 +2,40 @@ import React from "react";
 
 function Congestion() {
   return (
-    <div
-      style={{
-        backgroundImage: "url(/img/scroll-infograph-bg-5.webp)",
-        height: "100vh",
-      }}
-    >
-      Cutting Down Congestion
+    <div className="bg-[url('./img/scroll-infograph-bg-5.webp')] h-screen bg-no-repeat bg-cover">
+      <div className="h-full bg-black-rgba grid grid-cols-4">
+        <div></div>
+        <div className="col-span-2">
+          <div className="text-white mt-20 text-4xl grid grid-cols-2">
+            <h1 className="text-left">Cutting down Congestion </h1>
+            <h1 className="text-right">Padma Multipurpose Bridge</h1>
+          </div>
+          <div className="bg-red-500 h-1 w-full m-4"></div>
+          <div className="grid grid-cols-2 text-white mt-12">
+            <div>
+              <p className="text-2xl">
+                The Padma Multipurpose Bridge is just one of several large-scale
+                improvements to transportation that Vision 2041 cites as vital
+                for boosting trade, removing bottlenecks around major cities and
+                improving connections to rural areas
+              </p>
+            </div>
+            <div className="p-4 m-4">
+              <p className="text-3xl">
+                <p className="text-6xl">Vision 2041</p> Vital for boosting trade
+                and rapid economic growth
+              </p>
+            </div>
+          </div>
+          <div className="m-20">
+            <img
+              src="./img/scroll-infograph-thumbnail-1.webp"
+              alt="Padma Bridge"
+            />
+          </div>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
--- src/components/FutureCards.js
+++ src/components/FutureCards.js
@@ -4,40 +4,47 @@ function FutureCards() {
   return (
     <div className="bg-light-green">
       <div>
-        <h1 className="text-bd-green text-center py-40 text-bd font-semibold">
+        <h1 className="text-bd-green text-center py-40 px-96 mx-40 text-bd font-bold">
           Explore the Future of Opportunity in Bangladesh:
         </h1>
       </div>
-      <div className="flex mx-80">
+      <div className="flex mx-96 justify-center pb-40">
         <div className="bg-bd-green m-8 p-8 rounded w-60">
+          <h1 className="text-right font-bold text-3xl text-gray-700">01</h1>
           <h1 className="text-white">Building the Future</h1>
           <p className="text-white">
             How Mega-Infrastructure Projects Will Power Bangladesh's Economic
             Growth
           </p>
-          <button className="text-white p-4 rounded border-2">
+          <button className="text-white mx-2 px-6 py-2 font-bold rounded-2xl border-2 my-4">
             LEARN MORE
           </button>
-          <img src="./img/menu-thumbnail-3.webp" alt="Infrastructure" />
+          <img
+            // className="-mx-8 w-full"
+            src="./img/menu-thumbnail-3.webp"
+            alt="Infrastructure"
+          />
         </div>
         <div className="bg-red-600 m-8 p-8 rounded w-60">
+          <h1 className="text-right font-bold text-3xl text-gray-700">02</h1>
           <h1 className="text-white">Building the Future</h1>
           <p className="text-white">
             How Mega-Infrastructure Projects Will Power Bangladesh's Economic
             Growth
           </p>
-          <button className="text-white p-4 rounded border-2">
+          <button className="text-white px-6 py-2 font-bold rounded-2xl border-2 my-4">
             LEARN MORE
           </button>
           <img src="./img/menu-thumbnail-3.webp" alt="Infrastructure" />
         </div>
         <div className="bg-green-800 m-8 p-8 rounded w-60">
+          <h1 className="text-right font-bold text-3xl text-gray-700">03</h1>
           <h1 className="text-white">Building the Future</h1>
           <p className="text-white">
             How Mega-Infrastructure Projects Will Power Bangladesh's Economic
             Growth
           </p>
-          <button className="text-white p-4 rounded border-2">
+          <button className="text-white px-6 py-2 font-bold rounded-2xl border-2 my-4">
             LEARN MORE
           </button>
           <img src="./img/menu-thumbnail-3.webp" alt="Infrastructure" />
