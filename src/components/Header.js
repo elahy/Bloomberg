bab1487c908c91608f0b6702b4969cc4d1001272
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2022-04-14 21:34:48 +0600

    header mobile view

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,19 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="bg-[url('./img/tophead-img.webp')] bg-full">
+    <div className="xl:bg-[url('./img/tophead-img.webp')]">
       <div className="grid xl:grid-cols-2 h-full w-full animate">
-        <div className="h-96"></div>
-        <div className="h-full">
-          <div className="bg-white h-20 w-full">
-            <div className="bg-bd-green h-full animate-wiggle"></div>
+        <div className="h-80 bg-[url('./img/tophead.webp')] bg-cover xl:bg-none"></div>
+        <div className="xl:h-full">
+          <div className="bg-white h-10 xl:h-20 w-full">
+            <div className="bg-bd-green h-full" id="green"></div>
           </div>
           <div className="">
-            <p className="text-10xl pl-36 pr-44 pt-24  font-bold bg-white mix-blend-screen">
+            <p className="text-5xl xl:text-10xl pl-10 xl:pl-36 pr-10 xl:pr-44 pt-8 xl:pt-24 font-extrabold  xl:font-bold bg-white mix-blend-screen">
               THE RISE OF BENGAL TIGER
             </p>
           </div>
-          <div className="bg-white px-40 py-16">
+          <div className="bg-white px-28 xl:px-40 py-8 xl:py-16">
             <button className="bg-red-500 text-white font-semibold py-4 px-8 rounded">
               SCROLL
             </button>
